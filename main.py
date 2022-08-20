79b959479ac6d81e61b40f54ee8b5b99e4da66ae
Siphesihle <ssongang021@student.wethinkcode.co.za>
2022-08-20 12:58:35 +0200

    pixel creation

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import requests
 
+USERNAME = "sihle"
+TOKEN = "pwedyqwerty"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "pwedyqwerty",
-    "username": "sihle",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Coding Graph",
+    "unit": "hrs",
+    "type": "int",
+    "color": "momiji",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+
+# graph_response = requests.post(url=graph_endpoint, json=grap_config, headers=headers)
+# print(graph_response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20220720",
+    "quantity": "3"
+}
+
+pixel_response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(pixel_response.text)
+
+
+
